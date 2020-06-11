08ae85ff7212de42167c043ddc869ec97d116f79
facualavar <facualavar@gmail.com>
2020-06-11 01:28:27 -0300

    add notificaciones

--- notificaciones.php
+++ notificaciones.php
@@ -1,3 +1,32 @@
 <?php
 
+MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 
+switch($_POST["type"]) {
+    case "payment":
+        $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+        $archivo = fopen("json_notifications.txt", "a");
+        fputs($archivo, $payment);
+        fclose($archivo);
+        break;
+    case "plan":
+        $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
+        $archivo = fopen("json_notifications.txt", "a");
+        fputs($archivo, $plan);
+        fclose($archivo);
+        break;
+    case "subscription":
+        $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
+        $archivo = fopen("json_notifications.txt", "a");
+        fputs($archivo, $plan);
+        fclose($archivo);
+        break;
+    case "invoice":
+        $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+        $archivo = fopen("json_notifications.txt", "a");
+        fputs($archivo, $plan);
+        fclose($archivo);
+        break;
+}
+
+?>
